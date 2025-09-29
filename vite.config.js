96cad326804b9104b44f81af2c7d07a08d090929
Sinek1r <adolfo.torres@genstudents.org>
2025-09-29 20:39:06 +0000

    chore: configuración para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -5,4 +6,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   
+  base: '/react-posts-rubrica2/',
 })
